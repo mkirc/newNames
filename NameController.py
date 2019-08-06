2301b65deee3eb219e36817077409a373636cfd6
mkirc <m.p.kirchner@gmx.de>
2019-08-06 14:15:54 +0200

    thought GRASPed

--- NameController.py
+++ NameController.py
@@ -9,11 +9,13 @@ class NameControl:
 
 		self.rc = race
 		self.ln = random.choice(range(minlen, maxlen)) # Name length
-		self.vo = 'aeiou'
-		self.co = 'bcdfghjklmnpqrstvwxzjy'
+
 		self.nf = NameFactory()
 		self.cur = self.getNamebyRace()
 
+	def __add__(self, other):
+
+		return str(str(self.cur) + ' ' + str(other.cur))
 
 	def __str__(self):
 
@@ -22,172 +24,21 @@ class NameControl:
 	def getNamebyRace(self):
 
 		if self.rc == 'elf':
-			return self.returnElfName()
+			return self.nf.returnElfName(self.ln)
 		elif self.rc == 'orc':
-			return self.returnOrcName()
+			return self.nf.returnOrcName(self.ln)
 
 
-	def returnElfName(self):
-
-		x = True
-		while x:
-			count = 0
-			self.nf.getNameObj()
-			new = self.nf.populateNameObj(self.ln)
-			for pos,c in enumerate(str(new)):
-				if c in self.vo:
-					count += 1
-				if c in 'ozwkrtqx':
-					count -= 3
-				if c in 'dhalrin':
-					count += 2
-			try:
-				# check if next char is consonant
-				if c and str(new)[pos + 1] in self.co:
-						count -= 5
-			except IndexError as e:
-				pass
-			if str(new)[len(str(new)) - 1] in 'rnm':
-					count += 9
-
-			if count >= 15:
-				x = False
-
-		return capwords(str(new))	
-
-	def returnOrcName(self):
-
-		x = True
-		while x:
-			count = 0
-			self.nf.getNameObj()
-			new = self.nf.populateNameObj(self.ln)
-			for pos,c in enumerate(str(new)):
-				# check if char is vowel
-				if c in self.vo:
-					count += 1
-				if c in 'ourkbztfs':
-					count += 3
-				if c in 'aijyx':
-					count -= 10
-			try:
-				# check if next char is consonant
-				# makes assumtions about more readable names
-				if c and str(new)[pos + 1] in self.co:
-						count -= 5
-			except IndexError as e:
-				pass
-			if str(new)[len(str(new)) - 1] in 'rokg':
-					count += 10
-
-			if count >= 15:
-				x = False
-
-		return capwords(str(new))
 
 def ohneVieleWorte():
 
-	orc = NameControl('orc', 3 ,6)
-	print(orc)
+	newOrcName = NameControl('orc', 3 ,6)
+	newOrcSurname = NameControl('orc', 4, 7)
+	print(newOrcName + newOrcSurname)
 	for i in range(3):
-		newElf = NameControl('elf', 3, 6)
-		newElfSurname = NameControl('elf', 4, 7)
-		print(str(newElf) + ' ' + str(newElfSurname))
+		newElfName = NameControl('elf', 3, 6)
+		newElfSurname = NameControl('elf', 5, 8)
+		print(newElfName + newElfSurname)
 
 ohneVieleWorte()
 
-
-'''
-
-def main():
-
-	vovels = 'aeiou'
-	consonants = "bcdfghjklmnpqrstvwxzjy"
-	nf = NameFactory()
-	print('dotcom:')
-	for i in range(1):
-		x = True
-		while x:
-			count = 0
-			nf.getNameObj()
-			new = nf.makeNewName()
-			for pos,c in enumerate(str(new)):
-				# check if char is vowel
-				if c in vovels:
-					count += 2
-			try:
-				# check if next char is consonant
-				# makes assumtions about more readable names
-				if c and str(new)[pos + 1] in consonants:
-						count -= 9
-			except IndexError as e:
-				pass
-
-			if count >= 3:
-				x = False
-			suff = random.choice(['.net', '.io', '.com'])
-		print(' ' + capwords(str(new)) + suff)
-
-	print('orc:')
-	for i in range(3):
-		x = True
-		while x:
-			count = 0
-			nf.getNameObj()
-			new = nf.makeNewName()
-			for pos,c in enumerate(str(new)):
-				# check if char is vowel
-				if c in vovels:
-					count += 1
-				if c in 'ourkbztfs':
-					count += 3
-				if c in 'aijyx':
-					count -= 10
-			try:
-				# check if next char is consonant
-				# makes assumtions about more readable names
-				if c and str(new)[pos + 1] in consonants:
-						count -= 5
-			except IndexError as e:
-				pass
-			if str(new)[len(str(new)) - 1] in 'rokg':
-					count += 10
-
-			if count >= 15:
-				x = False
-
-		print(' ' + capwords(str(new)))
-
-	print('elf:')
-	for i in range(3):
-		x = True
-		while x:
-			count = 0
-			nf.getNameObj()
-			new = nf.makeNewName()
-			for pos,c in enumerate(str(new)):
-				# check if char is vowel
-				if c in vovels:
-					count += 1
-				if c in 'ozwkrtqx':
-					count -= 3
-				if c in 'dhalrin':
-					count += 2
-			try:
-				# check if next char is consonant
-				# makes assumtions about more readable names
-				if c and str(new)[pos + 1] in consonants:
-						count -= 5
-			except IndexError as e:
-				pass
-
-			if str(new)[len(str(new)) - 1] in 'rnm':
-					count += 9
-
-			if count >= 15:
-				x = False
-
-		print(' ' + capwords(str(new)))
-main()
-
-'''
--- makeName.py
+++ makeName.py
@@ -9,7 +9,8 @@ class NameFactory:
 
 		self.n = None
 		self.seed = 2451
-
+		self.vo = 'aeiou'
+		self.co = 'bcdfghjklmnpqrstvwxzjy'
 
 	def getNameObj(self):
 
@@ -28,6 +29,61 @@ class NameFactory:
 		self.n.out = nn # store name in Name() obj
 		return self.n
 		
+	def returnElfName(self, ln):
 
+		x = True
+		while x:
+			count = 0
+			self.getNameObj()
+			new = self.populateNameObj(ln)
+			for pos,c in enumerate(str(new)):
+				if c in self.vo:
+					count += 1
+				if c in 'ozwkrtqx':
+					count -= 3
+				if c in 'dhalrin':
+					count += 2
+			try:
+				# check if next char is consonant
+				if c and str(new)[pos + 1] in self.co:
+						count -= 5
+			except IndexError as e:
+				pass
+			if str(new)[len(str(new)) - 1] in 'rnm':
+					count += 9
 
+			if count >= 15:
+				x = False
+
+		return capwords(str(new))	
+
+	def returnOrcName(self, ln):
+
+		x = True
+		while x:
+			count = 0
+			self.getNameObj()
+			new = self.populateNameObj(ln)
+			for pos,c in enumerate(str(new)):
+				# check if char is vowel
+				if c in self.vo:
+					count += 1
+				if c in 'ourkbztfs':
+					count += 3
+				if c in 'aijyx':
+					count -= 10
+			try:
+				# check if next char is consonant
+				# makes assumtions about more readable names
+				if c and str(new)[pos + 1] in self.co:
+						count -= 5
+			except IndexError as e:
+				pass
+			if str(new)[len(str(new)) - 1] in 'rokg':
+					count += 10
+
+			if count >= 15:
+				x = False
+
+		return capwords(str(new))
 
